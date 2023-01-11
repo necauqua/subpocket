1686b6fe9cef93fbc686fc60d7c350197bf14121
Anton Bulakh <self@necauqua.dev>
2023-01-11 04:58:47 +0200

    Replace shell.nix with a devShell flake

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Just a devShell with a JVM, to use on NixOS with direnv";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = [ pkgs.jdk17 ];
+        };
+      }
+    );
+}
